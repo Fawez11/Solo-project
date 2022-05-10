c881dd699f9ba100e12a8eb8c946d220bfccb555
Fawez11 <fawezferjaoui11@gmail.com>
2022-05-10 10:17:43 +0200

    last push

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 var anime = document.querySelectorAll('.image')
+// taged the picture element
     
 function searchAnimeBYName() {
+    // create a function to filter the searches
     var searchAnime = document.getElementById("searchbox").value;
-
+//
     for (var i = 0; i < anime.length; i++) {
+        // loop througt the pictuers
         if (anime[i].getAttribute('alt').toLowerCase().includes(searchAnime.toLowerCase())) {
+            //we create an if condition to chek if our search is matching to what is wriiten in alt with the build in includes
+            //then we use the tolowercase to to make even upper or lower case 
             anime[i].classList.remove("is-hidden");
+            // remove the is hiden class to show the images
         } else {
             anime[i].classList.add("is-hidden");
         }
